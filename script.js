04f3bc7b7a3718b39bed49da64cfa1cddd1d3161
ruem1na <153555907+ruem1na@users.noreply.github.com>
2023-12-11 21:33:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
-const gif = document.querySelector("IMG_6146.gif");
+const gif = document.querySelector("https://media.discordapp.net/attachments/872496454114115644/1183866580854124694/IMG_6146.gif?ex=6589e4ac&is=65776fac&hm=5e5661fe35b8f6d8a0b82e971252b5a2ce262490958946ecfb0b6df7b61a56dc&.gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you soon!";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.discordapp.net/attachments/872496454114115644/1183866989375148164/IMG_6147.gif?ex=6589e50d&is=6577700d&hm=9dbc39e1b9bb3cb32c05a89ee305d0642d0dd499f7370b800fb01fedcd6b62b1&.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
