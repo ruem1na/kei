737c8cd231cdf16dd0fc2046d9b7763161b7b330
ruem1na <153555907+ruem1na@users.noreply.github.com>
2023-12-11 21:41:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
-const gif = document.querySelector("IMG_6146.gif");
+const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
